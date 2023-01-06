8ac388b623c6e675c8c9807c915d5ee791331037
Dao-Anh10 <hoanganhwms@gmail.com>
2023-01-06 10:05:39 +0700

    Revert "commit project training"
    This reverts commit 0b9a0debda74e62ee868f5e7e3fcbcbc62314768.

--- scripts/lib/Autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-function autoload($classPath) {
-    $classPath = explode('\\', $classPath);
-    $className = end($classPath);
-    array_pop($classPath);
-    include strtolower(implode('/', $classPath)) . '/' . $className . '.php';
-}
-
-spl_autoload_register('autoload');
--- site/view/page/error.php
+++
@@ -1,7 +0,0 @@
-<div id="alert">
-    <?php if (isset($this->data['message'])) { ?>
-        <div class="alert-page-error">
-            <strong>##404 <?php echo $this->data['message'] ?></strong>
-        </div>
-    <?php } ?>
-</div>
